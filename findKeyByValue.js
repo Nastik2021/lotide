87a6561ecd4223b88e5e99cac56d96c76ecc5ac3
Anastassia <nastiatsv@gmail.com>
2021-10-27 19:09:41 -0700

    more work on objects

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+
+const findKeyByValue = function(obj, val) {
+  const arrayOfKeys = Object.keys(obj); //stores keys of obj in this variable
+  // console.log(Object.keys(obj))
+  for (const keys of arrayOfKeys) {
+    if (obj[keys ]=== val) {
+      return keys;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
