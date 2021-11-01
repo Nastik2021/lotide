439f0abd93f4a26253c58a96e40056b69cd31622
Anastassia <nastiatsv@gmail.com>
2021-11-01 15:52:06 -0700

    refactoring

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,14 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("❌Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   }
   if (actual !== expected) {
-    console.log("✅Assertion Failed: " + actual + " !== " + expected);
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
 
   }
 };
 
+module.exports = assertEqual;
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hi", "hi");
-assertEqual("hi", "hello");
-assertEqual(2, 5);
 
 
